f0dd16f0ad07e4e1b1e0cfb85ce5d4cbf1f50b97
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-01 22:31:41 -0500

    [Update] Total complete H3

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+	return <h3 className='mt-3'>Total Complete Items: 5</h3>;
 };
 
 export default TotalCompleteItems;
