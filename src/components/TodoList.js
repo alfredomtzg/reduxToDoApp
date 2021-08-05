e8a3d94b05dc6bffdc08e46b8328214034c41627
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-04 23:20:05 -0500

    [Update] create action addTodo un adTodoForm

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ import TodoItem from './TodoItem';
 const TodoList = () => {
 	const todos = [
 		{ id: 1, title: 'todo1', completed: false },
-		{ id: 2, title: 'todo2', completed: false },
+		{ id: 2, title: 'todo2', completed: true },
 		{ id: 3, title: 'todo3', completed: true },
 		{ id: 4, title: 'todo4', completed: false },
 		{ id: 5, title: 'todo5', completed: false },
