05375b6446e1dcff7275f39603a7f1301bb1ca5d
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-04 23:52:56 -0500

    [Update] add handgle complete click in the checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleCompleted } from '../redux/ToDoSlice';
+
 
 const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch();
+	const handleCompleteClick = () => {
+		dispatch(toggleCompleted({id: id, completed: !completed}))
+	}
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
+					<input type='checkbox' 
+					className='mr-3' 
+					checked={completed}
+					onChange={handleCompleteClick}
+					></input>
 					{title}
 				</span>
 				<button className='btn btn-danger'>¡Delete!</button>
