48c045398feb2c2dac98db6198495c6554c6994f
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-04 23:46:21 -0500

    [Update] add toggleCompleted in reducers

--- src/redux/ToDoSlice.js
+++ src/redux/ToDoSlice.js
@@ -6,8 +6,8 @@ const todoSlice = createSlice({
     { id: 1, title: "todo1", completed: false },
     { id: 2, title: "todo2", completed: false },
     { id: 3, title: "todo3", completed: false },
-    { id: 4, title: "todo4", completed: false },
-    { id: 5, title: "todo5", completed: false },
+    { id: 4, title: "todo4", completed: true },
+    { id: 5, title: "todo 5", completed: false },
   ],
   reducers: {
     addTodo: (state, action) => {
@@ -17,10 +17,18 @@ const todoSlice = createSlice({
         completed: false
       };
       state.push(newTodo);
+    },
+    toggleCompleted: (state, action) => {
+      const index = state.findIndex(
+        (todo) => todo.id === action.payload.id);
+      state[index].completed = action.payload.completed;
     }
   }
 });
 
-export const { addTodo } = todoSlice.actions;
+export const {
+  addTodo,
+  toggleCompleted
+} = todoSlice.actions;
 
 export default todoSlice.reducer;
