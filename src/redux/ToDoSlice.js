79732851417954cccb95ae20b880f0598defa2cd
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-04 23:09:09 -0500

    [Update] create a createSlice from reduxjs toolkit

---
+++ src/redux/ToDoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: false },
+    { id: 4, title: "todo4", completed: false },
+    { id: 5, title: "todo5", completed: false },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        completed: false
+      };
+      state.push(newTodo);
+    }
+  }
+});
+
+export const { addTodo } = todoSlice.actions;
