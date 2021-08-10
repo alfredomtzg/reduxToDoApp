9a315b3637cc436e0d53c8224f917a561c780b6f
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-09 23:45:58 -0500

    [Update] add delete text

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,23 +6,24 @@ import { toggleCompleted, deleteTodo } from '../redux/ToDoSlice';
 const TodoItem = ({ id, title, completed }) => {
 	const dispatch = useDispatch();
 	const handleCompleteClick = () => {
-		dispatch(toggleCompleted({id: id, completed: !completed}))
+		dispatch(toggleCompleted({ id: id, completed: !completed }))
 	}
 	const handleDeleteClick = () => {
-		dispatch(deleteTodo({id: id}))
+		dispatch(deleteTodo({ id: id }))
 	}
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>
-					<input type='checkbox' 
-					className='mr-3' 
-					checked={completed}
-					onChange={handleCompleteClick}
+					<input type='checkbox'
+						className='mr-3'
+						checked={completed}
+						onChange={handleCompleteClick}
 					></input>
 					{title}
 				</span>
-				<button  onClick={handleDeleteClick} className='btn btn-danger'>¡Delete!</button>
+				<button onClick={handleDeleteClick} className='btn btn-danger'>¡Delete!!
+				</button>
 			</div>
 		</li>
 	);
