3866a46ae1267ae219ed826534f3945873a12f87
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-08-11 22:20:51 -0500

    [Update] useEfect whit getTodosAsunc function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoItem from './TodoItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTodosAsync } from '../redux/ToDoSlice';
 
 const TodoList = () => {
-	const todos = useSelector((state)=> state.todos)
+	const dispatch = useDispatch();
+	const todos = useSelector((state) => state.todos);
+
+	useEffect(() => {
+		dispatch(getTodosAsync())
+	}, [dispatch]);
 
 	return (
 		<ul className='list-group'>
